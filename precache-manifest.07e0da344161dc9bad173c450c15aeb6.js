7200609404680ce93559eb6a1c4ea9f7b4cd8a13
Katharine Cooney <katharinecooney@gmail.com>
2019-08-12 14:51:47 -0500

    Updates

---
+++ precache-manifest.07e0da344161dc9bad173c450c15aeb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836e7cdd1cb1b0da43fd5d90a0450dda",
+    "url": "/yandex-translator-app/index.html"
+  },
+  {
+    "revision": "fa2d97dd82292c97c64e",
+    "url": "/yandex-translator-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "668eedd238f05d083b37",
+    "url": "/yandex-translator-app/static/js/2.568fa783.chunk.js"
+  },
+  {
+    "revision": "fa2d97dd82292c97c64e",
+    "url": "/yandex-translator-app/static/js/main.da786e5e.chunk.js"
+  },
+  {
+    "revision": "1dcc77f879bb984325bb",
+    "url": "/yandex-translator-app/static/js/runtime~main.9c8c1b51.js"
+  }
+]);
