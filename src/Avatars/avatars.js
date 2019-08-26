c0e31c3ee8c6147acacb780149cdfb875e513249
Katharine Cooney <katharinecooney@gmail.com>
2019-08-26 14:06:33 -0500

    Remove an avatar due to improper height

--- src/Avatars/avatars.js
+++ src/Avatars/avatars.js
@@ -4,14 +4,13 @@ import Girl3 from './Girl3.png';
 import Girl4 from './Girl4.png';
 import Girl5 from './Girl5.png';
 import Girl6 from './Girl6.png';
-import Girl7 from './Girl7.png';
 import Boy1 from './Boy1.png';
 import Boy2 from './Boy2.png';
 import Boy3 from './Boy3.png';
 import Boy4 from './Boy4.png';
 import Boy5 from './Boy5.png';
 
-const avatars = [Girl1, Girl2, Girl3, Girl4, Girl5, Girl6, Girl7, Boy1, Boy2, Boy3, Boy4, Boy5];
+const avatars = [Girl1, Girl2, Girl3, Girl4, Girl5, Girl6, Boy1, Boy2, Boy3, Boy4, Boy5];
 
 const randomIndex1 = Math.floor(Math.random() * avatars.length);
 let avatar1 = avatars.splice(randomIndex1, 1);
